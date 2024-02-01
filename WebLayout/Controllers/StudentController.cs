b692c86269c754a690914d00aba5521d152530fa
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-01 14:34:02 +0530

    Add project files.

---
+++ WebLayout/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLayout.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Student Home Page";
+
+            return View();
+        }
+
+        public ActionResult NameList()
+        {
+            List<string> list = new List<string>()
+            {
+                "Ravi",
+                "Raj",
+                "Nitu",
+                "Ria"
+            };
+            ViewBag.nameList = list;
+
+            return View();
+        }
+    }
+}
---
+++ WebLayout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebLayout.MvcApplication" Language="C#" %>
